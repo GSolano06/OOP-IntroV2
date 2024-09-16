ff8463d9aaffe69d2a93dceba1bfa77420876fc7
GSolano <gabesolano06@gmail.com>
2024-09-16 10:27:25 -0400

    gabe

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
