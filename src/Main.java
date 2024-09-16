70fac960c684a2866f36e9bfc431eeb6af63c537
tnunesueno <tnunesueno25@scienceleadership.org>
2024-09-16 09:49:05 -0400

    twyla

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,16 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Twyla and Dylan!");
 
+        Team GoldenStateWarriors = new Team();
 
+        GoldenStateWarriors.name = "Golden State Warriors";
+        GoldenStateWarriors.home = "San Francisco";
+        GoldenStateWarriors.numPlayers = 22;
+        GoldenStateWarriors.league ="NBA";
+        GoldenStateWarriors.sport = "Basketball";
+        GoldenStateWarriors.college = false;
+        GoldenStateWarriors.numCoaches = 10;
+
+    GoldenStateWarriors.describe();
     }
 }
---
+++ src/Team.java
@@ -0,0 +1,27 @@
+public class Team {
+
+    String league;
+    String sport;
+    boolean college;
+    String name;
+    int numPlayers;
+    int numCoaches;
+    String home;
+    int winRecord;
+    int lossRecord;
+
+
+    void describe(){
+    System.out.println("Team: "+ name +" Home: " + home + " Sport: "+ sport +" League: " + league + " Current Players: " + numPlayers +" Coaches: "+ numCoaches);
+
+    };
+    void updateRecord(boolean gameWon) {
+        if (gameWon=true) {
+            winRecord = winRecord + 1;
+        } else {
+            lossRecord=lossRecord+1;
+        }
+    }
+   // void addPlayer(Player newPlayer){
+    //};
+}
